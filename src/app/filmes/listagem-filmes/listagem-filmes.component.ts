fd77ebfbdbd28410d1204dcb688abe218597d0b2
AugustoCesar <augustocesar.udi@gmail.com>
2021-08-02 20:59:02 -0300

    Filter, Order

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
 
@@ -11,22 +12,49 @@ export class ListagemFilmesComponent implements OnInit {
 
   readonly qtdPagina = 4;
   pagina = 0;
+  texto: string;
+  genero: string;
   filmes: Filme[] = [];
+  filtrosListagem: FormGroup;
+  generos: Array<string>;
 
-  constructor(private filmesService: FilmesService) { }
+  constructor(private filmesService: FilmesService,
+    private fb: FormBuilder) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.filtrosListagem = this.fb.group({
+      texto: [''],
+      genero: ['']
+    });
+
+    this.filtrosListagem.get('texto').valueChanges.subscribe((val: string) => {
+      this.texto = val;
+      this.resetarConsulta();
+    });
+
+    this.filtrosListagem.get('genero').valueChanges.subscribe((val: string) => {
+      this.genero = val;
+      this.resetarConsulta();
+    });
+
+    this.generos = ['Ação', 'Romance', 'Aventura', 'Terror', 'Ficção científica', 'Comédia', 'Drama'];
+
     this.listarFilmes();
   }
 
   onScroll(): void {
     this.listarFilmes();
-    console.log('scrolled!!');
   }
 
   private listarFilmes(): void {
     this.pagina++;
-    this.filmesService.listar(this.pagina, this.qtdPagina)
-        .subscribe((filmes: Filme[]) => this.filmes.push(...filmes));
+    this.filmesService.listar(this.pagina, this.qtdPagina, this.texto, this.genero)
+      .subscribe((filmes: Filme[]) => this.filmes.push(...filmes));
+  }
+
+  private resetarConsulta(): void {
+    this.pagina = 0;
+    this.filmes = [];
+    this.listarFilmes();
   }
 }
